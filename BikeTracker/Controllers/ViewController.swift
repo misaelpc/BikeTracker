71793a69afab8c77cd8e1630c9792926518e47cb
Misael Pérez Chamorro <misaelperezchamorro@MacBook-Pro-de-Misael.local>
2022-08-26 10:14:47 -0500

    Setup Project and run linter

---
+++ BikeTracker/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BikeTracker
+//
+//  Created by Misael Pérez Chamorro on 26/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
