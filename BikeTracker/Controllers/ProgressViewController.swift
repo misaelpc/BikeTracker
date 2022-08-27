fbfe6479b88031c225af7b54e08e373f0e9596d7
Misael Pérez Chamorro <misaelperezchamorro@MacBook-Pro-de-Misael.local>
2022-08-27 09:49:32 -0500

    Setup Home Tab Bar Controller

---
+++ BikeTracker/Controllers/ProgressViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ProgressViewController.swift
+//  BikeTracker
+//
+//  Created by Misael Pérez Chamorro on 27/08/22.
+//
+
+import UIKit
+
+class ProgressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
